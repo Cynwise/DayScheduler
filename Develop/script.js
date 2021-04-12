964d8b5a9c754564a574709b2dfdf33ca659cf8f
William Farnsworth <williamcoreyf@gmail.com>
2021-04-12 15:43:32 -0400

    implemented save button and local storage functionality

--- Develop/script.js
+++ Develop/script.js
@@ -5,5 +5,25 @@ $(document).ready(function(){
     // Current Day Displayed
     $("#currentDay").text(m.format("dddd, MMMM Do YYYY"));
 
+    // Loading saved calendar data if exists
+    $("#9am .data").val(localStorage.getItem("9am"));
+    $("#10am .data").val(localStorage.getItem("10am"));
+    $("#11am .data").val(localStorage.getItem("11am"));
+    $("#12pm .data").val(localStorage.getItem("12pm"));
+    $("#1pm .data").val(localStorage.getItem("1pm"));
+    $("#2pm .data").val(localStorage.getItem("2pm"));
+    $("#3pm .data").val(localStorage.getItem("3pm"));
+    $("#4pm .data").val(localStorage.getItem("4pm"));
+    $("#5pm .data").val(localStorage.getItem("5pm"));
 
+    // Save button functionality
+    $( "button.saveBtn" ).on( "click", function() {
+        console.log( "click" );
+
+        let saveData = $(this).siblings(".data").val();;
+        let saveTime = $(this).parent().attr("id");
+
+        localStorage.setItem(saveTime, saveData);
+        // localStorage.lastname = "Smith";
+    });
 });
