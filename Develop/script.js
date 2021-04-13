b964f08b8cea8d8980b4dc93416d9e78fed18be6
William Farnsworth <williamcoreyf@gmail.com>
2021-04-13 12:20:46 -0400

    implemented past, present, and future time colors

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,7 @@
 const m = moment();
 let currentDay = $('#currentDay');
+let currentTime = moment().format('H');
+let hourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 $(document).ready(function(){
 
@@ -11,30 +13,41 @@ $(document).ready(function(){
     }, 1000);
 
     // Loading saved calendar data if exists
-    $("#9am .data").val(localStorage.getItem("9am"));
-    $("#10am .data").val(localStorage.getItem("10am"));
-    $("#11am .data").val(localStorage.getItem("11am"));
-    $("#12pm .data").val(localStorage.getItem("12pm"));
-    $("#1pm .data").val(localStorage.getItem("1pm"));
-    $("#2pm .data").val(localStorage.getItem("2pm"));
-    $("#3pm .data").val(localStorage.getItem("3pm"));
-    $("#4pm .data").val(localStorage.getItem("4pm"));
-    $("#5pm .data").val(localStorage.getItem("5pm"));
+    $("#9 .data").val(localStorage.getItem("9"));
+    $("#10 .data").val(localStorage.getItem("10"));
+    $("#11 .data").val(localStorage.getItem("11"));
+    $("#12 .data").val(localStorage.getItem("12"));
+    $("#13 .data").val(localStorage.getItem("13"));
+    $("#14 .data").val(localStorage.getItem("14"));
+    $("#15 .data").val(localStorage.getItem("15"));
+    $("#16 .data").val(localStorage.getItem("16"));
+    $("#17 .data").val(localStorage.getItem("17"));
 
     // Save button functionality
     $( "button.saveBtn" ).on( "click", function() {
-        console.log( "click" );
-
         let saveData = $(this).siblings(".data").val();;
         let saveTime = $(this).parent().attr("id");
 
         localStorage.setItem(saveTime, saveData);
-        // localStorage.lastname = "Smith";
     });
 
-    function updateTime() {
-
-
-
+    // checks current array index against current time and colors it accordingly
+    function checkTime(time) {
+        if (time < parseInt(currentTime)) {
+            $("#" + hourArray[i]).addClass('past');
+        }else if (time > parseInt(currentTime)){
+            $("#" + hourArray[i]).addClass('future');
+        }else if (time == parseInt(currentTime)){
+            $("#" + hourArray[i]).addClass('present');
+        }
     }
+    
+    function crawlTime() {
+        for (i = 0; i < hourArray.length; i++) {
+            checkTime(hourArray[i]);    
+        } 
+    };
+
+    crawlTime();
+    
 });
